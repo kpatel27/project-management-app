e8d9a09bac5d7e087c973bcf32e1ff7fd1d1ab96
Krunal Patel <kppatel727@gmail.com>
2020-06-26 10:04:27 -0700

    added list title edit form

--- app/javascript/components/list/List.js
+++ app/javascript/components/list/List.js
@@ -1,41 +1,72 @@
-import React from 'react';
-import CardListingContainer from '../card-listing/CardListingContainer';
-import AddCardContainer from '../add-card/AddCardContainer';
+import React, { Component } from "react";
+import CardListingContainer from "../card-listing/CardListingContainer";
+import AddCardContainer from "../add-card/AddCardContainer";
 
-const List = props => {
-  return (
-    <div className='list-wrapper'>
-      <div className='list-background'>
-        <div className='list'>
-          <a className='more-icon sm-icon' href=''></a>
-          <div>
-            <p className='list-title'>{props.title}</p>
-          </div>
-          <div className='add-dropdown add-top'>
-            <div className='card'></div>
-            <a className='button'>Add</a>
-            <i className='x-icon icon'></i>
-            <div className='add-options'>
-              <span>...</span>
+class List extends Component {
+  state = {
+    title: this.props.title,
+    showEditForm: false,
+  };
+
+  showEditForm = (e) => {
+    this.setState({ showEditForm: !this.state.showEditForm });
+    console.log(e.target);
+  };
+
+  handleChange = (e) => {
+    this.setState({ title: e.target.value });
+    console.log("hi");
+  };
+
+  render() {
+    return (
+      <div className="list-wrapper">
+        <div className="list-background">
+          <div className="list">
+            <a className="more-icon sm-icon" href=""></a>
+            <div>
+              {this.state.showEditForm ? (
+                <input
+                  type="text"
+                  className="list-title"
+                  value={this.state.title}
+                  onChange={this.handleChange}
+                />
+              ) : (
+                <p className="list-title" onClick={this.showEditForm}>
+                  {this.props.title}
+                </p>
+              )}
             </div>
-          </div>
-          <CardListingContainer listId={props.id} />
-          <div className='add-dropdown add-bottom'>
-            <div className='card'>
-              <div className='card-info'></div>
-              <textarea name='add-card'></textarea>
-              <div className='members'></div>
+            <div className="add-dropdown add-top">
+              <div className="card"></div>
+              <a className="button">Add</a>
+              <i className="x-icon icon"></i>
+              <div className="add-options">
+                <span>...</span>
+              </div>
             </div>
-            <a className='button'>Add</a>
-            <i className='x-icon icon'></i>
-            <div className='add-options'>
-              <span>...</span>
+            <CardListingContainer listId={this.props.id} />
+            <div className="add-dropdown add-bottom">
+              <div className="card">
+                <div className="card-info"></div>
+                <textarea name="add-card"></textarea>
+                <div className="members"></div>
+              </div>
+              <a className="button">Add</a>
+              <i className="x-icon icon"></i>
+              <div className="add-options">
+                <span>...</span>
+              </div>
             </div>
+            <AddCardContainer
+              boardId={this.props.board_id}
+              listId={this.props.id}
+            />
           </div>
-          <AddCardContainer boardId={props.board_id} listId={props.id} />
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 export default List;
